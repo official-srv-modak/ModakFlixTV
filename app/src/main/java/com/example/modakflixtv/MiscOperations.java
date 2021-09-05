19759cacc92c3422593f95a473cb9c1bea852007
Sourav Modak <72427070+official-srv-modak@users.noreply.github.com>
2021-09-05 14:13:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/modakflixtv/MiscOperations.java
@@ -0,0 +1,111 @@
+package com.example.modakflixtv;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MiscOperations {
+
+    public static String ip = "modakflix.com", ipInfoFilePath = "";
+
+    public static String domain_name = "http://"+ip+"/";
+    public static String record_position_path = domain_name+"record_position.php";
+    public static String delete_position_path = domain_name+"delete_from_shows_watched.php";
+    public static String get_shows_watched_path = domain_name+"get_shows_watched.php?username=admin";
+    public static String reset_profile = domain_name+"reset_profile.php?username=admin";
+    public static String get_movies_list = domain_name+"get_movies_list_json.php";
+    public static String reload_shows_watched = domain_name+"reload_shows_watched.php";
+    public static String search_shows = domain_name+"search_show.php";
+    public static String get_profiles = domain_name+"get_profiles.php";
+    public static String reload_description = domain_name+"reload_description.php";
+    public static String get_description = domain_name+"get_description.php";
+    public static String add_profile = domain_name+"add_profile.php";
+
+    public static JSONObject getDataFromServer(String URL)
+    {
+        URL = handleUrl(URL);
+        String output = "";
+        Log.e("URL", URL);
+        try{
+            java.net.URL url = new URL(URL);
+            Map params = new LinkedHashMap<>();
+            StringBuilder postData = new StringBuilder();
+            Set<Map.Entry> s = params.entrySet();
+            for (Map.Entry param : s) {
+                if (postData.length() != 0) postData.append('&');
+                postData.append(URLEncoder.encode((String) param.getKey(), "UTF-8"));
+                postData.append('=');
+                postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+            }
+            byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+            conn.setDoOutput(true);
+            conn.getOutputStream().write(postDataBytes);
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                {
+                    output += inputLine;
+                }
+            }
+            in.close();
+            JSONObject jsonObj = new JSONObject(output);
+            return jsonObj;
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            Log.e("URL", URL);
+            return null;
+        }
+    }
+    public static String handleUrl(String URL)
+    {
+        if(!URL.isEmpty())
+        {
+            String output = "http:";
+            //Log.e("YY", URL);
+            String [] splitList = URL.split(output);
+            if(splitList.length>1)
+            {
+                String temp = splitList[1];
+                temp = temp.replace("/", "forwardslash");
+                temp = temp.replace(" ", "spacebarspace");
+                temp = temp.replace("?", "questionmarkquestion");
+                temp = temp.replace("&", "emparsandemparsand");
+                temp = temp.replace("=", "equaltoequal");
+                try {
+                    temp= URLEncoder.encode(temp, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                temp = temp.replace("forwardslash", "/");
+                temp = temp.replace("spacebarspace", "%20");
+                temp = temp.replace("questionmarkquestion", "?");
+                temp = temp.replace("emparsandemparsand", "&");
+                temp = temp.replace("equaltoequal", "=");
+
+                output += temp;
+                return output;
+            }
+            else
+                return URL;
+        }
+        else
+            return URL;
+
+    }
+}
---
+++ app/src/main/java/com/example/modakflixtv/MovieList.java
@@ -0,0 +1,86 @@
+package com.example.modakflixtv;
+
+import static com.example.modakflixtv.MainFragment.NUM_COLS;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class MovieList {
+    public static final String MOVIE_CATEGORY[] = {
+            "Movies",
+    };
+
+    private static List<Movie> list;
+    private static long count = 0;
+
+    public static List<Movie> getList() {
+        if (list == null) {
+            list = setupMovies();
+        }
+        return list;
+    }
+
+    public static List<Movie> setupMovies() {
+        list = new ArrayList<>();
+
+        JSONObject showJSONList = MiscOperations.getDataFromServer(MiscOperations.get_movies_list);
+
+        String title[] = getListsFromJSONObject(showJSONList, "name");
+        NUM_COLS = title.length;
+
+        String description[] = getListsFromJSONObject(showJSONList, "des");;
+        String videoUrl[] = getListsFromJSONObject(showJSONList, "url");
+        String bgImageUrl[] = getListsFromJSONObject(showJSONList, "album_art_path");
+        String cardImageUrl[] = getListsFromJSONObject(showJSONList, "album_art_path");
+
+        for (int index = 0; index < cardImageUrl.length; ++index) {
+            list.add(
+                    buildMovieInfo(
+                            title[index],
+                            description[index],
+                            videoUrl[index],
+                            cardImageUrl[index],
+                            bgImageUrl[index]));
+        }
+
+        return list;
+    }
+
+    private static Movie buildMovieInfo(
+            String title,
+            String description,
+            String videoUrl,
+            String cardImageUrl,
+            String backgroundImageUrl) {
+        Movie movie = new Movie();
+        movie.setId(count++);
+        movie.setTitle(title);
+        movie.setDescription(description);
+        movie.setCardImageUrl(cardImageUrl);
+        movie.setBackgroundImageUrl(backgroundImageUrl);
+        movie.setVideoUrl(videoUrl);
+        return movie;
+    }
+
+    private static String[] getListsFromJSONObject(JSONObject jsonObject, String index)
+    {
+        try {
+            JSONArray cards = jsonObject.getJSONArray("cards");
+            String[] output = new String[cards.length()];
+
+            for(int i = 0; i < cards.length(); i++)
+            {
+                JSONObject temp = (JSONObject) cards.get(i);
+                output[i] = temp.getString(index);
+            }
+            return output;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
